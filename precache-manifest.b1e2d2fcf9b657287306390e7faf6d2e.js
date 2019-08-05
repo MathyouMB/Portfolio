88967404af4c2d33632e2ad2f71381f45960c05f
Matthew M-B <43223682+MathyouMB@users.noreply.github.com>
2019-08-05 19:16:49 -0400

    Updates

---
+++ precache-manifest.b1e2d2fcf9b657287306390e7faf6d2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a51a9d3c69432421227d45e752caed72",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "17705257dfd884905eae",
+    "url": "/portfolio/static/css/main.9e7ffaf4.chunk.css"
+  },
+  {
+    "revision": "56f431d2bf2ef8aa5972",
+    "url": "/portfolio/static/js/2.72b2c803.chunk.js"
+  },
+  {
+    "revision": "17705257dfd884905eae",
+    "url": "/portfolio/static/js/main.b45b523a.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
