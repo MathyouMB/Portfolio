885a64bff6dbb0bb4b75e0e716ab60778878cd2f
Matthew M-B <matthewmacraebovell@outlook.com>
2019-09-04 09:27:52 -0400

    Updates

---
+++ precache-manifest.6287ff23d3fec3ded4a4bd61db0b7030.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9db54b88834eb6dbf91dad9508088fd2",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "9df3b52438b5eae6cacd",
+    "url": "/Portfolio/static/css/main.222721c4.chunk.css"
+  },
+  {
+    "revision": "56f431d2bf2ef8aa5972",
+    "url": "/Portfolio/static/js/2.72b2c803.chunk.js"
+  },
+  {
+    "revision": "9df3b52438b5eae6cacd",
+    "url": "/Portfolio/static/js/main.b78ede2a.chunk.js"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  }
+]);
